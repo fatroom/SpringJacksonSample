36b552a0fdf0d9385e54272fc3c015df718c1bd9
Roman Romanchuk <fatroom@gmail.com>
2011-08-24 00:23:32 +0300

    Custom date formatter added

--- src/main/java/org/noisyteam/samples/spring/jackson/json/CustomObjectMapper.java
+++ src/main/java/org/noisyteam/samples/spring/jackson/json/CustomObjectMapper.java
@@ -4,11 +4,16 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ser.StdSerializerProvider;
 import org.springframework.stereotype.Component;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 @Component
 public class CustomObjectMapper extends ObjectMapper {
     public CustomObjectMapper() {
+        DateFormat format = new SimpleDateFormat("yyyy-mm-dd hh:MM");
         StdSerializerProvider sp = new StdSerializerProvider();
         sp.setNullValueSerializer(new NullSerializer());
         this.setSerializerProvider(sp);
+        this.getSerializationConfig().setDateFormat(format);
     }
 }
