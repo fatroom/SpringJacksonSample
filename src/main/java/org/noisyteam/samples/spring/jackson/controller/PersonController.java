a078efb84875ca675f62d686e0586807a8f5acce
Roman Romanchuk <fatroom@gmail.com>
2011-08-23 23:02:28 +0300

    Base spring module added

---
+++ src/main/java/org/noisyteam/samples/spring/jackson/controller/PersonController.java
@@ -0,0 +1,45 @@
+package org.noisyteam.samples.spring.jackson.controller;
+
+import javax.validation.Valid;
+import org.noisyteam.samples.spring.jackson.model.Person;
+import org.noisyteam.samples.spring.jackson.validator.PersonValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.validation.BindingResult;
+
+/**
+ * Handles requests for the application home page.
+ *
+ * @author Roman Romanchuk (fatroom@gmail.com)
+ */
+@Controller
+public class PersonController {
+    private static final Logger logger = LoggerFactory.getLogger(PersonController.class);
+
+    private static final String DEFAULT_VIEW = "WEB-INF/views/home.jsp";
+
+    @InitBinder("person")
+    protected void initBinder(WebDataBinder binder) {
+        binder.setValidator(new PersonValidator());
+    }
+
+    @RequestMapping(value = "/", method=RequestMethod.GET)
+    public String home(Model model) {
+        logger.trace("Passing through...");
+        model.addAttribute("person", new Person());
+        return DEFAULT_VIEW;
+    }
+
+    @RequestMapping(value = "/", method=RequestMethod.POST)
+    public String create(@Valid Person person, BindingResult result, Model model) {
+        logger.trace("Saving action");
+        model.addAttribute("person", person);
+        return DEFAULT_VIEW;
+    }
+}
